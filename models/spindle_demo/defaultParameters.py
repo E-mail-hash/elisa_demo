4b939059030ec42f079975199bdc70e4e3a13ee1
Email <2562520735@qq.com>
2025-12-03 17:30:38 +0800

    251203

--- models/spindle_demo/defaultParameters.py
+++ models/spindle_demo/defaultParameters.py
@@ -3,14 +3,15 @@ import numpy as np
 def loadDefaultParams(seed=None):
     class struct(object):
         pass 
-    params = struct
+    params = struct()
 
     params.dt = 1
-    params.duration = 5 * 1000
+    params.duration = 10 * 1000
+    params.model = "spindle1"
     
     # model parameters
-    params.tau_t = 0.044
-    params.tau_r = 0.022
+    params.tau_t = 0.044 * 1000
+    params.tau_r = 0.022* 1000
 
     params.f_t_max = 250
     params.f_r_max = 200
@@ -18,15 +19,15 @@ def loadDefaultParams(seed=None):
     params.f_t_th = 50
     params.f_r_th = 110
 
-    params.gamma_t = 0.005
-    params.gamma_r = 0.005
+    params.gamma_t = 0.25#0.005 # 0.25 # change as HTC
+    params.gamma_r = 0.25#0.005 # 0.25# change as HTC
 
-    params.At = 0.22
-    params.Ar = 0.11
+    params.At = 0.22 * 1000
+    params.Ar = 0.11 * 1000
 
     ###
-    params.tau_u_t = 0.22
-    params.tau_u_r = 0.11
+    params.tau_u_t = 0.22* 1000
+    params.tau_u_r = 0.11* 1000
     params.RT_t = 100
     params.RT_r = 180
     params.RB_t = 250
@@ -43,20 +44,20 @@ def loadDefaultParams(seed=None):
     params.Lt = 0.04
     params.Lr = 0.04
 
-    params.Nt = 0.5 * 1000
+    params.Nt = 0.5 * 1000 * 2
     params.Nr = 0.5 * 1000
 
     params.Prr = 0.22
     params.Ptr = 0.01
     params.Prt = 0.04
 
-    params.Jrr = -0.09
+    params.Jrr =  -0.09 #1.5 # change as HTC
     params.Jtr = 3.42
-    params.Jrt = -0.5
+    params.Jrt = -0.5 #1.6# change as HTC
 
     # initialization
-    params.V_t_init =  0
-    params.V_r_init =  0
+    params.V_t_init = -67
+    params.V_r_init = -67
 
     params.Q_t_init = 0
     params.Q_r_init = 0
--- models/spindle_demo/timeIntegration.py
+++ models/spindle_demo/timeIntegration.py
@@ -3,24 +3,57 @@ for spindles
 """
 import numpy as np
 import numba
-import defaultparameters as dp
+import defaultParameters as dp
 
 def timeintegration(params):
     # simulation parameters
-    # dt = params["dt"]
-    # duration = params["duration"]
-    # rngseed = params["seed"]
-
-    for par in params.keys():
-        exec(f'{par} = params["{par}"]')
+    dt = params["dt"]
+    duration = params["duration"]
 
     t = np.arange(1, round(duration, 6)/dt+1)
 
     # neuron parameters
     startind = 1 
 
-    # tau_t = params["tau_t"]
-    # tau_r = params["tau_r"]
+    tau_t = params["tau_t"]
+    tau_r = params["tau_r"]
+
+    f_t_max = params["f_t_max"]
+    f_r_max = params["f_r_max"]
+    f_t_th = params["f_t_th"]
+    f_r_th = params["f_r_th"]
+    gamma_t = params["gamma_t"]
+    gamma_r = params["gamma_r"]
+    At = params["At"]
+    Ar = params["Ar"]
+
+    tau_u_t = params["tau_u_t"]
+    tau_u_r = params["tau_u_r"]
+    RT_t = params["RT_t"]
+    RT_r = params["RT_r"]
+    VT_t = params["VT_t"]
+    VT_r = params["VT_r"]
+    gT_t = params["gT_t"]
+    gT_r = params["gT_r"]
+    Lt = params["Lt"]
+    Lr = params["Lr"]
+    RB_t = params["RB_t"]
+    RB_r = params["RB_r"]
+    VB_t = params["VB_t"]
+    VB_r = params["VB_r"]
+    gB_t = params["gB_t"]
+    gB_r = params["gB_r"]   
+
+    Nt = params["Nt"]
+    Nr = params["Nr"]
+    Prr = params["Prr"]
+    Ptr = params["Ptr"]
+    Prt = params["Prt"]
+    Jrr = params["Jrr"]
+    Jtr = params["Jtr"]
+    Jrt = params["Jrt"]
+
+
 
 
     # neuron initialization
@@ -34,6 +67,10 @@ def timeintegration(params):
     u_t = float(params["u_t_init"])
     u_r = float(params["u_r_init"])
 
+    a_chunk = np.zeros(len(t))
+    b_chunk = np.zeros(len(t))
+    c_chunk = np.zeros(len(t))
+
     return timeIntegration_njit_elementwise(  
     startind, t, dt,
     V_t, tau_t, f_t_max, f_t_th, gamma_t, At,
@@ -43,12 +80,12 @@ def timeintegration(params):
     RT_t, VT_t, gT_t, Lt, RB_t, VB_t, gB_t,
     RT_r, VT_r, gT_r, Lr, RB_r, VB_r, gB_r,
     Q_t, Q_r,
-    Nr, Nt, Prr, Ptr, Prt, Jrr, Jtr, Jrt,
-    )
+    a_chunk, b_chunk, c_chunk,
+    Nr, Nt, Prr, Ptr, Prt, Jrr, Jtr, Jrt)
 
 
 
-@numba.njit()
+# @numba.njit()
 def timeIntegration_njit_elementwise(
     startind, t, dt,
     V_t, tau_t, f_t_max, f_t_th, gamma_t, At,
@@ -58,13 +95,13 @@ def timeIntegration_njit_elementwise(
     RT_t, VT_t, gT_t, Lt, RB_t, VB_t, gB_t,
     RT_r, VT_r, gT_r, Lr, RB_r, VB_r, gB_r,
     Q_t, Q_r,
-    Nr, Nt, Prr, Ptr, Prt, Jrr, Jtr, Jrt,
-):
+    a_chunk, b_chunk, c_chunk,
+    Nr, Nt, Prr, Ptr, Prt, Jrr, Jtr, Jrt):
     def _function_u(u, m):
         if m == "t":
-            f_max = f_t_max
-            f_th = f_t_th
-            gamma = gamma_t
+            f_max = f_t_max # 250
+            f_th = f_t_th # 50
+            gamma = gamma_t # 0.005
         elif m == "r":
             f_max = f_r_max
             f_th = f_r_th
@@ -72,13 +109,14 @@ def timeIntegration_njit_elementwise(
         return -f_max / (1 + np.exp((u+f_th)/gamma))
     
     def _function_b(V, m):
+        Balance = 0
         if m == "t":
-            if V >= -0.1:
+            if V-Balance >= -0.1:# change as HTC
                 b = 0
             else:
                 b = -200
         elif m == "r":
-            if V > 0:
+            if V-Balance > 0:# change as HTC
                 b = 0
             else:
                 b = -200
@@ -86,13 +124,13 @@ def timeIntegration_njit_elementwise(
 
     def _function_Q(V, u, m):
         if m == "t":
-            RT = RT_t
-            VT = VT_t
-            gT = gT_t
-            L = Lt 
-            RB = RB_t
-            VB = VB_t
-            gB = gB_t
+            RT = RT_t # 100
+            VT = VT_t # 26
+            gT = gT_t # 6
+            L = Lt  # 0.04
+            RB = RB_t # 250
+            VB = VB_t # 15
+            gB = gB_t # 0.9
         elif m == "r":
             RT = RT_r
             VT = VT_r
@@ -107,11 +145,21 @@ def timeIntegration_njit_elementwise(
 
     # derivatives
     for i in range(startind, len(t)):
+
+        Q_t[i-1] = _function_Q(V_t[i-1], u_t, "t")
+        Q_r[i-1] = _function_Q(V_r[i-1], u_r, "r")
         
 
         # r.h.s
-        V_t_rhs = -V_t[i-1] / tau_t + _function_u(u_t, "t") / At + Nr*Prt*Jrt*Q_r[i-1]
-        V_r_rhs = -V_r[i-1] / rau_r + _function_u(u_r, "r") / Ar + Nr*Prr*Jrr*Q_r[i-1] + Nt*Ptr*Jtr*Q_t[i-1]
+        a = -V_t[i-1] / tau_t
+        b = _function_u(u_t, "t") / At
+        c = Nr*Prt*Jrt*Q_r[i-1] / 1000
+        V_t_rhs = a + b + c
+        a_chunk[i] = a
+        b_chunk[i] = b
+        c_chunk[i] = c
+        # V_t_rhs = -V_t[i-1] / tau_t + _function_u(u_t, "t") / At + Nr*Prt*Jrt*Q_r[i-1] 
+        V_r_rhs = -V_r[i-1] / rau_r + _function_u(u_r, "r") / Ar + Nr*Prr*Jrr*Q_r[i-1] / 1000  + Nt*Ptr*Jtr*Q_t[i-1] / 1000
 
         u_t_rhs = (_function_b(V_t[i-1], "t") - u_t) / tau_u_t
         u_r_rhs = (_function_b(V_r[i-1], "r") - u_r) / tau_u_r
@@ -123,10 +171,8 @@ def timeIntegration_njit_elementwise(
         u_t = u_t + dt * u_t_rhs
         u_r = u_r + dt * u_r_rhs
 
-        Q_t[i] = _function_Q(V_t[i], u_t, "t")
-        Q_r[i] = _function_Q(V_r[i], u_r, "r")
-    
+   
     
 
-    return Q_t, Q_r, V_t, V_r, t
+    return Q_t, Q_r, V_t, V_r, t, a_chunk, b_chunk, c_chunk
 
